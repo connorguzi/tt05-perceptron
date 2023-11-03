bcc5148e4d89001d18f8232c6ece994d486f26dc
Connor <connor@pop-os.localdomain>
2023-11-03 15:58:55 -0700

    Added pseudo bnn

--- src/bnn.v
+++ src/bnn.v
@@ -7,52 +7,53 @@ module bnn (
     reg [3:0] out_reg;
     reg [6:0] result0, result1, result2, result3, result4, result5, result6, result7, result8, result9;
     reg [6:0] weight0, weight1, weight2, weight3, weight4, weight5, weight6, weight7, weight8, weight9;
-    assign weight0 = 7b'1111011;
-    assign weight1 = 7b'1101111;
-    assign weight2 = 7b'1011101;
-    assign weight3 = 7b'1101011;
-    assign weight4 = 7b'1001111;
-    assign weight5 = 7b'1011100;
-    assign weight6 = 7b'1111010;
-    assign weight7 = 7b'1011111;
-    assign weight8 = 7b'1110111;
-    assign weight9 = 7b'1101100;
+    assign weight0 = 7'b1111011;
+    assign weight1 = 7'b1101111;
+    assign weight2 = 7'b1011101;
+    assign weight3 = 7'b1101011;
+    assign weight4 = 7'b1001111;
+    assign weight5 = 7'b1011100;
+    assign weight6 = 7'b1111010;
+    assign weight7 = 7'b1011111;
+    assign weight8 = 7'b1110111;
+    assign weight9 = 7'b1101100;
 
-    assign result0 = weight0 & in;
-    assign result1 = weight1 & in;
-    assign result2 = weight2 & in;
-    assign result3 = weight3 & in;
-    assign result4 = weight4 & in;
-    assign result5 = weight5 & in;
-    assign result6 = weight6 & in;
-    assign result7 = weight7 & in;
-    assign result8 = weight8 & in;
-    assign result9 = weight9 & in;
+    assign result0 = ~(weight0 ^ in);
+    assign result1 = ~(weight1 ^ in);
+    assign result2 = ~(weight2 ^ in);
+    assign result3 = ~(weight3 ^ in);
+    assign result4 = ~(weight4 ^ in);
+    assign result5 = ~(weight5 ^ in);
+    assign result6 = ~(weight6 ^ in);
+    assign result7 = ~(weight7 ^ in);
+    assign result8 = ~(weight8 ^ in);
+    assign result9 = ~(weight9 ^ in);
 
     always @(*) begin
         if(result0 == 0)begin
             out_reg = 4'b00;
         end
         else if(result1 == 0)begin
-            out_reg = 4'b1;
+            out_reg = 4'b0001;
         end
         else if(result2 == 0)begin
-            out_reg = 4'b10;
+            out_reg = 4'b0010;
+            // out_reg = 4'b0111;
         end
         else if(result3 == 0)begin
-            out_reg = 4'b11;
+            out_reg = 4'b0011;
         end
         else if(result4 == 0)begin
-            out_reg = 4'b100;
+            out_reg = 4'b0100;
         end
         else if(result5 == 0)begin
-            out_reg = 4'b101;
+            out_reg = 4'b0101;
         end
         else if(result6 == 0)begin
-            out_reg = 4'b110;
+            out_reg = 4'b0110;
         end
         else if(result7 == 0)begin
-            out_reg = 4'b111;
+            out_reg = 4'b0111;
         end
         else if(result8 == 0)begin
             out_reg = 4'b1000;
--- src/test.py
+++ src/test.py
@@ -7,8 +7,8 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 # Numbers in array are in order 0-9 with a bogus test input for the last one
 input_arr = [0b0000100, 0b0010000, 0b0100010, 0b0010100, 0b0110000, 0b0100011, 0b0000101, 0b0100000, 0b0001000, 0b0010011, 0b0111011]
 
-# Labels of expected outputs given inputs, if given a bogus input, the result should be 31
-input_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 31] 
+# Labels of expected outputs given inputs, if given a bogus input, the result should be 15
+input_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15] 
 @cocotb.test()
 async def test_my_design(dut):
 
@@ -22,10 +22,12 @@ async def test_my_design(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
+
     for i in range(len(input_arr)):
         dut.ui_in.value = input_arr[i]
         await ClockCycles(dut.clk, 5)
-        assert dut.uo_out.value == input_labels[i]
-        dut._log.info(f"Passed test for {input_labels[i]}")
+        assert dut.uo_out.value & 0b1111 == input_labels[i]
+        assert (dut.uo_out.value >> 4) == input_labels[i]
+        dut._log.info(f"Passed test for {input_labels[i] if input_labels[i] < 10 else 'bogus'}")
 
     dut._log.info("Finished test")
